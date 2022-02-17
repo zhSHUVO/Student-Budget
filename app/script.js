fb8fa99f77e4c6b7e348c99850f11f47001a6f8a
zhSHUVO <zhshuvo99@gmail.com>
2022-02-17 14:57:15 +0600

    save calculation

--- app/script.js
+++ app/script.js
@@ -1,27 +1,40 @@
+// theses are global variable cause we'r going to need them in more than one function
+let totalIncome;
+let foodCost;
+let rentCost;
+let clothsCost;
+let remainingBalance;
+let saveTarget
 
 
 // expenses & remaining calculation
 function calculateExpenses() {
-    // taking user values
-    let totalIncome = parseFloat(document.getElementById("income").value);
-    let foodCost = parseFloat(document.getElementById("food").value);
-    let rentCost = parseFloat(document.getElementById("rent").value);
-    let clothsCost = parseFloat(document.getElementById("cloths").value);
+    // taking user input value
+    totalIncome = parseFloat(document.getElementById("income").value);
+    foodCost = parseFloat(document.getElementById("food").value);
+    rentCost = parseFloat(document.getElementById("rent").value);
+    clothsCost = parseFloat(document.getElementById("cloths").value);
+
 
     // calculating  expenses and remaining
     let totalExpenses = foodCost + rentCost + clothsCost;
-    let remainingBalance = totalIncome - totalExpenses;
+    remainingBalance = totalIncome - totalExpenses;
 
     // showing the results to the user
     document.getElementById("monthly-balance").innerHTML = totalIncome;
     document.getElementById("total-expenses").innerHTML = totalExpenses;
     document.getElementById("remaining-balance").innerHTML = remainingBalance;
-
-    // cleaning the input fields
-    document.getElementById("income").value = "";
-    document.getElementById("food").value = "";
-    document.getElementById("rent").value = "";
-    document.getElementById("cloths").value = "";
-
 }
 
+function calculateSave() {
+    // taking user input value
+    saveTarget = document.getElementById("save-target-amount").value;
+
+    // calculating save and remainig 
+    let savedAmount = totalIncome * saveTarget / 100;
+    let afterSave = remainingBalance - savedAmount;
+
+    // showing the results to the user
+    document.getElementById("saved-amount").innerHTML = savedAmount;
+    document.getElementById("after-save-balance").innerHTML = afterSave;
+}
